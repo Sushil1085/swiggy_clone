31c4aca899f93da130fa9f4b5ec5466572723fe7
Sushil Patil <sushilpatil1085@gmail.com>
2024-10-08 12:28:51 +0530

    Add files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+const Contact =()=>{
+    return(
+        <div>
+            <h1>For More details</h1>
+            <h2>contact us <br></br> sushilpatil1085@gmail.com</h2>
+        </div>
+    )
+}
+export default Contact;
